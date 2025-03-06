c79eaca9097307bb863798818f8ce13af921d4a9
Richard Darwin Sy <toro.richarddarwin.sy@gmail.com>
2025-03-06 14:41:54 +0800

    Added Headers

--- client/src/pages/Contact.tsx
+++ client/src/pages/Contact.tsx
@@ -1,6 +1,9 @@
+import PageHeader from "../components/PageHeader";
+
 const Contact = () => {
   return (
     <>
+      <PageHeader text={"Contact Page"} />
       <h1>This is the Contact Page</h1>
     </>
   );
--- client/src/pages/Shop.tsx
+++ client/src/pages/Shop.tsx
@@ -1,6 +1,9 @@
+import PageHeader from "../components/PageHeader";
+
 const Shop = () => {
   return (
     <>
+      <PageHeader text={"Shop page"} />
       <h1>This is the shopping page</h1>
     </>
   );
