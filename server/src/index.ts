60d4d251748b8903b7b6d0c27414d1d4a01f3008
Rich1068 <rs106848@gmail.com>
2025-03-14 01:02:20 +0800

    cors fix

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,9 +16,9 @@ const app = express();
 
 app.use(
   cors({
-    origin: `${process.env.CLIENT_URL}`,
+    origin: process.env.CLIENT_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
