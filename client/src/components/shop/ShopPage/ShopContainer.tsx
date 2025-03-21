74eb4598982ef4b8bd7f80bbab01d091f187eed0
Richard Darwin Sy <toro.richarddarwin.sy@gmail.com>
2025-03-21 10:24:28 +0800

    .

--- client/src/components/shop/ShopPage/ShopContainer.tsx
+++ client/src/components/shop/ShopPage/ShopContainer.tsx
@@ -75,7 +75,7 @@ const ShopContainer: FC<{ allProducts: IProduct[] }> = ({ allProducts }) => {
           />
         </div>
       )}
-      <div className="flex flex-row min-h-svh shrink">
+      <div className="flex flex-row shrink">
         <div className="relative max-md:hidden min-w-50 max-w-64 w-full">
           <ShopFilter
             selected={selected}
