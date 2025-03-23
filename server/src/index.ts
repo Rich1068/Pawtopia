04a09e8b1d1343de5e8b07061f5d7e2808a69afa
Rich1068 <rs106848@gmail.com>
2025-03-23 20:23:53 +0800

    vercel fixing

--- server/src/index.ts
+++ server/src/index.ts
@@ -19,7 +19,7 @@ const app = express();
 app.use(
   cors({
     origin: [process.env.CLIENT_URL!, "https://checkout.stripe.com"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type"],
     credentials: true,
   })
